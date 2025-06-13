a7cfeece1db3a2a0570522853c2eeb9ce2dcc409
5HT <maxim@synrc.com>
2025-06-13 03:05:52 +0300

    incorporate encrypt/decrypt into client and server message loop

--- skynet.c
+++ skynet.c
@@ -445,12 +445,6 @@ static void handle_message(ServerState *state, NodeState *node, SkyNetMessage *m
 
     skynet_print(msg);
 
-    char from_name[16];
-    snprintf(from_name, sizeof(from_name), "%08x", msg->node_id);
-
-    char to_name[16];
-    snprintf(to_name, sizeof(to_name), "%08x", msg->npg_id);
-
     const char *topic = NULL;
     switch (msg->npg_id) {
         case SKYNET_NPG_CONTROL: topic = "npg_control"; break;
@@ -466,37 +460,32 @@ static void handle_message(ServerState *state, NodeState *node, SkyNetMessage *m
             return;
     }
 
-
-    char topic_str[16];
     uint32_t topic_hash = fnv1a_32(topic, strlen(topic));
+    uint32_t to = msg->npg_id;
+    uint32_t from = msg->node_id;
+
+    char from_name[16];
+    char to_name[16];
+    char topic_str[16];
+
+    snprintf(from_name, sizeof(from_name), "%08x", msg->node_id);
+    snprintf(to_name, sizeof(to_name), "%08x", msg->npg_id);
     snprintf(topic_str, 16, "%08x", topic_hash);
 
-    uint32_t to = (msg->npg_id == SKYNET_NPG_CONTROL ? 0x40ac3dd2 : topic_hash);
-
-    printf("RCVD [NPG:%d][%s][seq:%u][node:%x][type:%d][src:%s:%d][server:%s][to:%x]\n",
-           msg->npg_id, topic, msg->seq_no, msg->node_id, msg->type,
-           inet_ntoa(addr->sin_addr), ntohs(addr->sin_port), state->server_name, to);
-
-    if (is_duplicate(state, msg->node_id, msg->seq_no, msg->type, addr)) {
-        return;
-    }
+    if (is_duplicate(state, msg->node_id, msg->seq_no, msg->type, addr)) { return; }
 
     if (msg->type == SKYNET_MSG_KEY_EXCHANGE && msg->npg_id == SKYNET_NPG_CONTROL) {
 
-        // don't decrypt
-
-//     } else {
-
-        // decrypt
-
-        hex_dump("SKY HEX DUMP", (const uint8_t *)msg, 309);
-
-        if (skynet_decrypt(1, msg, to, msg->node_id) < 0) {
-            fprintf(stderr, "Decryption failed for node %u, seq=%u\n", msg->node_id, msg->seq_no);
-            return;
-        }
+        to = (msg->npg_id == SKYNET_NPG_CONTROL ? 0x40ac3dd2 : topic_hash);
 
     }
+
+    if (skynet_decrypt(1, msg, to, msg->node_id) < 0) {
+        fprintf(stderr, "Decryption failed for node %u, seq=%u\n", msg->node_id, msg->seq_no);
+        return;
+    }
+
+    hex_dump("SKY HEX DUMP", (const uint8_t *)msg, 309);
 
     switch (msg->type) {
         case SKYNET_MSG_PUBLIC:
