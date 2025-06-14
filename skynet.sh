a513ec55df7c402e901ff02dbce615ab6d006784
5HT <maxim@synrc.com>
2025-06-15 02:40:19 +0300

    wip

--- skynet.sh
+++ skynet.sh
@@ -1,6 +1,6 @@
 rm -f skynet skynet_client skynet_decrypt skynet_encrypt skynet_keygen
 
-gcc -o skynet          skynet.c         skynet_proto.c -lcrypto
+gcc -o skynet          skynet.c         skynet_proto.c -lcrypto -pthread
 gcc -o skynet_client   skynet_client.c  skynet_proto.c -lcrypto
 gcc -o skynet_decrypt  skynet_decrypt.c skynet_proto.c -lcrypto
 gcc -o skynet_encrypt  skynet_encrypt.c skynet_proto.c -lcrypto
@@ -17,7 +17,7 @@ gcc -o skynet_keygen   skynet_keygen.c  skynet_proto.c -lcrypto
 ./skynet_keygen server           --server
 ./skynet_keygen client           --client
 
+cp /home/user/.skynet/ecc/secp384r1/*.ec_pub /home/user/.skynet_client/ecc/secp384r1/
+
 ./skynet_encrypt client server 1
 ./skynet_decrypt client server 1.sky
-
-
